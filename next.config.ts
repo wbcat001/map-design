5bd27a3e34c32e31395ad798738f8b294e283a62
aceron-tech <68225198+aceron-tech@users.noreply.github.com>
2025-02-23 20:37:57 +0900

    change config: standalone

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
