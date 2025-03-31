ae20d7ed40b079d8fefca7755c2647e7ba7b6c32
aceron-tech <68225198+aceron-tech@users.noreply.github.com>
2025-03-31 12:39:16 +0900

    build

---
+++ src/components/Icon/LikeIcon.tsx
@@ -0,0 +1,8 @@
+import {SVGProps} from "react";
+
+
+export function LikeIcon(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" fillRule="evenodd" d="M4.536 5.778a5 5 0 0 1 7.07 0q.275.274.708.682q.432-.408.707-.682a5 5 0 0 1 7.125 7.016L13.02 19.92a1 1 0 0 1-1.414 0L4.48 12.795a5 5 0 0 1 .055-7.017z"></path></svg>
+    )
+  }
